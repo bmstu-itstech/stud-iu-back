3ffe6b2dc877e158860f6efb1e736a8b241e260b
Elkin Kirill <kirillv.2016@mail.ru>
2026-08-19 14:37:49 +0300

    future: add admin panel for events app

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,1 +1,69 @@
 from django.contrib import admin
+
+from .models import EventImages, FutureEvents, PastEvents
+
+
+class PastEventImagesInline(admin.TabularInline):
+    model = EventImages
+    fk_name = "past_event"
+    extra = 1
+    fields = ("image",)
+
+
+class FutureEventImagesInline(admin.TabularInline):
+    model = EventImages
+    fk_name = "future_event"
+    extra = 1
+    fields = ("image",)
+
+
+@admin.register(PastEvents)
+class PastEventsAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "place",
+        "get_date_range",
+        "precision",
+        "has_album",
+        "images_count",
+    )
+    search_fields = ("title", "description", "place")
+    inlines = [PastEventImagesInline]
+
+    @admin.display(description="Даты проведения")
+    def get_date_range(self, obj):
+        return obj.date_range_display
+
+    @admin.display(description="Альбом", boolean=True)
+    def has_album(self, obj):
+        return bool(obj.album_link)
+
+    @admin.display(description="Фото")
+    def images_count(self, obj):
+        return obj.images.count()
+
+
+@admin.register(FutureEvents)
+class FutureEventsAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "place",
+        "get_date_range",
+        "precision",
+        "has_registration",
+        "images_count",
+    )
+    search_fields = ("title", "description", "place")
+    inlines = [FutureEventImagesInline]
+
+    @admin.display(description="Даты проведения")
+    def get_date_range(self, obj):
+        return obj.date_range_display
+
+    @admin.display(description="Регистрация", boolean=True)
+    def has_registration(self, obj):
+        return bool(obj.registration_link)
+
+    @admin.display(description="Фото")
+    def images_count(self, obj):
+        return obj.images.count()
